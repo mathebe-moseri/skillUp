ad92e016bb469a36a21e6468a11b17fea037c63f
mathebe <mathebemoseri@gmail.com>
2026-02-23 09:57:50 +0200

    refactor: organize landing page components and add landing page images

---
+++ src/app/landing/components/dashboard-preview/dashboard-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-preview.component.html',
+  styleUrl: './dashboard-preview.component.css'
+})
+export class DashboardPreviewComponent {
+
+}
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LandingComponent } from '../landing/landing.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [LandingComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
