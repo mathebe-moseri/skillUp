0de20b1fd614d569ad68ed3545b46dd2fb773312
mathebe <mathebemoseri@gmail.com>
2026-03-09 09:02:48 +0200

    nav(modal): add avaiable to courses

--- src/app/landing/components/hero/hero.component.ts
+++ src/app/landing/components/hero/hero.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from "../../../shared/ui/button/button.component";
 import { UiStateService } from '../../../shared/state/ui-state.service';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
